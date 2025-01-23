2b25c89d19271be687fbe5204fec5202de31a4eb
Carlos Domani <carlos.domani@torchtechnologies.com>
2025-01-23 09:58:04 -0600

    Cleanup

--- auditorium/frontend/src/index.tsx
+++ auditorium/frontend/src/index.tsx
@@ -4,8 +4,9 @@ import NewSymphony from "./pages/NewSymphony";
 import EditSymphony from "./pages/EditSymphony";
 import NewNote from "./pages/NewNote";
 import EditNote from "./pages/EditNote";
+import "./index.css";
 
-function App() {
+export default function App() {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4">Symphony Manager</h1>
@@ -19,5 +20,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
--- auditorium/frontend/src/main.tsx
+++ auditorium/frontend/src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
 import { SymphoniesProvider } from "./lib/SymphonyProvider.tsx";
+import App from "./index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
